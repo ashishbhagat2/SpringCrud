7ed50b105a98d067d7b393eb10e46945467bffcc
Ashish Bhagat <ashish.bhagat@hotmail.com>
2021-11-27 18:23:42 +0530

    Added Open API

--- SpringCrud/src/main/java/com/abhagat/SpringCrud/SpringCrudApplication.java
+++ SpringCrud/src/main/java/com/abhagat/SpringCrud/SpringCrudApplication.java
@@ -3,7 +3,11 @@ package com.abhagat.SpringCrud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info= @Info(title ="Product API", version="3.0", description="Employee Microservice"))
 public class SpringCrudApplication {
 
 	public static void main(String[] args) {
